738a332d072aa1b26588e1665f45b57a4364579a
AizSave <aiz.save@gmail.com>
2025-03-09 19:45:32 +0100

    v0.5.1.11

--- src/main/java/aphorea/items/weapons/melee/sword/VoidHammer.java
+++ src/main/java/aphorea/items/weapons/melee/sword/VoidHammer.java
@@ -4,10 +4,13 @@ import aphorea.items.vanillaitemtypes.weapons.AphHeavyHammerProjectileToolItem;
 import aphorea.projectiles.toolitem.VoidStoneProjectile;
 import necesse.engine.localization.Localization;
 import necesse.engine.network.PacketReader;
+import necesse.engine.network.gameNetworkData.GNDItemMap;
 import necesse.engine.network.packet.PacketSpawnProjectile;
 import necesse.engine.util.GameBlackboard;
 import necesse.engine.util.GameRandom;
 import necesse.entity.mobs.PlayerMob;
+import necesse.entity.mobs.itemAttacker.ItemAttackSlot;
+import necesse.entity.mobs.itemAttacker.ItemAttackerMob;
 import necesse.entity.projectile.Projectile;
 import necesse.entity.projectile.modifiers.ResilienceOnHitProjectileModifier;
 import necesse.gfx.gameTooltips.ListGameTooltips;
@@ -21,13 +24,14 @@ public class VoidHammer extends AphHeavyHammerProjectileToolItem {
         this.attackRange.setBaseValue(400);
     }
 
-    public InventoryItem onAttack(Level level, int x, int y, PlayerMob player, int attackHeight, InventoryItem item, PlayerInventorySlot slot, int animAttack, int seed, PacketReader contentReader) {
-        Projectile projectile = new VoidStoneProjectile(player, player.x, player.y, (float) x, (float) y, (float) this.getProjectileVelocity(item, player), this.getAttackRange(item), this.getAttackDamage(item), this.getKnockback(item, player));
+    @Override
+    public InventoryItem onAttack(Level level, int x, int y, ItemAttackerMob attackerMob, int attackHeight, InventoryItem item, ItemAttackSlot slot, int animAttack, int seed, GNDItemMap mapContent) {
+        Projectile projectile = new VoidStoneProjectile(attackerMob, attackerMob.x, attackerMob.y, (float) x, (float) y, (float) this.getProjectileVelocity(item, attackerMob), this.getAttackRange(item), this.getAttackDamage(item), this.getKnockback(item, attackerMob));
         projectile.setModifier(new ResilienceOnHitProjectileModifier(this.getResilienceGain(item)));
         projectile.resetUniqueID(new GameRandom(seed));
         level.entityManager.projectiles.addHidden(projectile);
         if (level.isServer()) {
-            level.getServer().network.sendToClientsWithEntityExcept(new PacketSpawnProjectile(projectile), projectile, player.getServerClient());
+            level.getServer().network.sendToAllClients(new PacketSpawnProjectile(projectile));
         }
 
         return item;
